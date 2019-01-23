2bd8654b65bf86cf90e2903ddf4f3bbc278b49ed
NikitinMike <jobles11@gmail.com>
2019-01-24 00:44:23 +0300

    react app update

--- reacttt/src/App.jsx
+++ reacttt/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 // import './bootstrap.css'
 import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { Provider } from 'react-globally'
+// import {gs} from 'react-global-state';
 
 import Field from './Field';
 // import Welcome from './Welcome';
@@ -8,40 +11,15 @@ import Field from './Field';
 
 // function Welcome(props) { return <h1 hidden>Hello, {props.name}!</h1>;}
 
-const dimension=3;
-const size=dimension*dimension-1;
+export const dimension=3;
 
 class App extends React.Component {
-
-  state = { disabled: [],table : [] } 
-
-  componentWillMount(){
-    const {table,disabled} = this.state
-    for(var i=0;i<=size;i++) {
-      disabled[i]=false;
-      table[i]=i;
-    }
-    this.setState(table);
-    this.setState(disabled);
-    // console.log(this.state.table);
-    // console.log(this.state.disabled);
-  }
-
-  menuBarClick = (e) => {
-    // e.preventDefault();
-    const item=e.target.innerText
-    const {disabled} = this.state
-    disabled[item]=!disabled[item];
-    console.log(item,disabled[item])
-    this.setState(disabled);
-    // this.render()
-  }
 
   render () {
     return (
       <div className='App'>
       {/* <Welcome name="Kitty"/> */}
-      <Field maxWidth={50*dimension} table={this.state.table} click={this.menuBarClick}/>
+      <Field maxWidth={50*dimension} dimension={dimension}/>
       </div>
     )
   }
--- reacttt/src/Field.jsx
+++ reacttt/src/Field.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
 
+import {dimension} from './App'
+var size=dimension*dimension-1;
+ 
 class Cell extends React.Component {
 
+    state = { disabled:false } 
+
+    moveClick = (e) => {
+        // e.preventDefault();
+        // const item=e.target.innerText
+        var {disabled} = this.state
+        disabled=!disabled;
+        // disabled=true;
+        // console.log(item,disabled)
+        this.setState({disabled});
+        // this.render()
+        e.target.innerText="X"
+        this.move(e,0)
+    }
+
+    move(e,n) {
+        if(n>size) return;
+        const cell=Math.floor(Math.random()*size);
+        const item=e.target.parentNode.childNodes[cell]
+        // console.log(cell,e.target.parentNode.childNodes[cell])
+        if (item.innerText) this.move(e,n+1)
+        else item.innerText="O"
+    }
+
     render () {
-        const disabled = false
-        // console.log(this.props.item);
-        // disabled = this.state.disabled[this.props.item]
-        console.log(this.state)
         return (
-            <button className="flex-itm" onClick={this.props.click} disabled={disabled}>
-                {this.props.item}
+            <button className="flex-itm" onClick={this.moveClick} disabled={this.state.disabled}>
+                {/* &nbsp; */}
+                {/* {this.props.item} */}
             </button>
         )
     }
@@ -18,10 +42,20 @@ class Cell extends React.Component {
 
 class Field extends React.Component {
 
+    state = {table : [] } 
+
+    componentWillMount(){
+        size=dimension*dimension-1;
+        const {table} = this.state
+        for(var i=0;i<=size;i++) table[i]=i;
+        this.setState(table);
+        // console.log(this.state.table);
+    }
+
     render () {
         return (
             <div className="flex" style={{maxWidth: this.props.maxWidth}}>
-                {this.props.table.map(item => <Cell key={item} item={item} click={this.props.click}/>)}
+                {this.state.table.map(item => <Cell key={item} item={item} />)}
             </div>
         )
     }
