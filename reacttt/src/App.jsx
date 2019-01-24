cbab2410efc39225b896f28a89a89d7eeaa5b63e
mike <mike@digt.ru>
2019-01-24 17:42:31 +0300

    resize gameboard menu

--- reacttt/src/App.jsx
+++ reacttt/src/App.jsx
@@ -5,19 +5,25 @@ import React from 'react'
 // import { Provider } from 'react-globally'
 // import {gs} from 'react-global-state';
 
+import MyMenu from './Menu'
 import Field from './Field';
 // import Welcome from './Welcome';
 // import BigTitle from './BigTitle';
 
 function Welcome(props) { return <h1 hidden>Hello, {props.name}!</h1>;}
 
-export const dimension=3;
+export const dimension=4;
 
 class App extends React.Component {
+
+  menuBarClick = (item) => {
+    console.log(item)
+  }
 
   render () {
     return (
       <div className='App'>
+        <MyMenu menu={["3","4","5","6","7","8","9"]} click={this.menuBarClick}/>
       { <Welcome name="Kitty"/> }
       <Field maxWidth={50*dimension} dimension={dimension}/>
       </div>
