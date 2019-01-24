83740edef2a6a28b71d1db67d35c61be8f5986db
NikitinMike <jobles11@gmail.com>
2019-01-25 00:33:15 +0300

    clear

--- src/main/java/com/example/demo/MovesService.java
+++ src/main/java/com/example/demo/MovesService.java
@@ -11,11 +11,6 @@ public class MovesService {
     @Autowired MovesRepository movesRepository;
 
     public Move create(int cell, int turn, int round, int row, int col, int user) {
-//        List <Move> moves = movesRepository.findAllByRound(round);
-//        moves.forEach((m)->System.out.print(m.getCell()+","));
-//        List <Move> moves = movesRepository.findAllByCellAndRound(cell,round);
-//        if(!moves.isEmpty()) return null;
-//        System.out.println("MOVE:"+row+","+col);
         Move move=new Move(cell,turn,round,row,col,user);
         movesRepository.save(move);
         return move;
