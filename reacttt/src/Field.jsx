acfe17cce27e6114dd52c8133202b74e8d87cd6c
NikitinMike <jobles11@gmail.com>
2019-01-24 05:39:29 +0300

    save state var as STATE

--- reacttt/src/Field.jsx
+++ reacttt/src/Field.jsx
@@ -2,24 +2,26 @@ import React from 'react'
 
 import {dimension} from './App'
 var SIZE=dimension*dimension;
- 
+const STATE = {table:[]}
+
 class Cell extends React.Component {
 
     state = { disabled:false } 
 
     moveClick = (e) => {
-        console.log(e.target.innerText)
+        // console.log(e.target.innerText)
         // if (e.target.innerText) return;
-        // e.preventDefault();
         // const item=e.target.innerText
         var {disabled} = this.state
         disabled=!disabled;
         // disabled=true;
-        // console.log(item,disabled)
+        console.log(">",e.target.id)
         this.setState({disabled});
         // this.render()
         e.target.innerText="X"
         this.move(e,0)
+        STATE[e.target.id]='x';
+        console.log(STATE);
     }
 
     move(e,n) {
@@ -28,12 +30,16 @@ class Cell extends React.Component {
         const item=e.target.parentNode.childNodes[cell]
         // console.log(cell,e.target.parentNode.childNodes[cell])
         if (item.innerText) this.move(e,n+1)
-        else item.innerText="O"
+        else {
+            item.innerText="O"
+            console.log("<",item.id)
+            STATE[item.id]='o';
+        }
     }
 
     render () {
         return (
-            <button className="flex-itm" onClick={this.moveClick} disabled={this.state.disabled}>
+            <button className="flex-itm" onClick={this.moveClick} disabled={this.state.disabled} id={this.props.item}>
                 {/* &nbsp; */}
                 {/* {this.props.item} */}
             </button>
@@ -47,6 +53,7 @@ class Field extends React.Component {
     state = {table:[]}
 
     componentWillMount(){
+        // e.preventDefault();
         SIZE=dimension*dimension;
         const {table} = this.state
         for(var i=0;i<SIZE;i++) table[i]=i;
@@ -55,6 +62,8 @@ class Field extends React.Component {
     }
 
     render () {
+        // e.preventDefault();
+        // console.log(this.state)
         return (
             <div className="flex" style={{maxWidth: this.props.maxWidth}}>
                 {this.state.table.map(item => <Cell key={item} item={item} />)}
