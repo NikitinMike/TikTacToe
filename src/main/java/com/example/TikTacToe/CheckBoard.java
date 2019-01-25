1daf3801eea66a4a4225a2cc30be1976ee7eab83
NikitinMike <jobles11@gmail.com>
2019-01-25 18:33:08 +0300

    check board were optimised

--- src/main/java/com/example/TikTacToe/CheckBoard.java
+++ src/main/java/com/example/TikTacToe/CheckBoard.java
@@ -1,6 +1,5 @@
 package com.example.TikTacToe;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -8,16 +7,15 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 import java.util.List;
 
-
 @Controller
 @CrossOrigin // (origins = "http://localhost:3000")
 public class CheckBoard {
 
     @Autowired
     MovesService moves;
+
     int dim=3;
     int[][]board;
-    int user=1;
 
     void showBoard(){
         System.out.println();
@@ -44,6 +42,7 @@ public class CheckBoard {
     @RequestMapping(value = "/check/{round}", method = RequestMethod.GET, produces = "application/json")
     public String check(@PathVariable int round){
         dim=round/1000;
+        int user=1;
         List<Move> turns=moves.getUserRound(user,round);
 //        turns.forEach(t-> System.out.println(t));
         board = new int[dim][dim];
@@ -54,7 +53,7 @@ public class CheckBoard {
         if(checkRowCol(user)) return "{\"success\":1}"; // "YOU WIN by ROW!"
 
         boolean d1=true,d2=true;
-        for (int i=0;i<dim;i++) {// check main diag
+        for (int i=0;i<dim;i++) {// check both diag
             if (board[i][i] != user) d1 = false;
             if (board[i][dim-i-1] != user) d2 = false;
         }
