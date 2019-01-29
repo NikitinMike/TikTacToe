8e11b6cf7e2eaf6c1217cfcce0caf5116fe093a6
NikitinMike <jobles11@gmail.com>
2019-01-29 22:52:39 +0300

    well done

---
+++ reacttt/build/precache-manifest.894f8218a60b7121e5b68f88da097648.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "09ca05edf55c87b5d6d4",
+    "url": "/static/js/main.09ca05ed.chunk.js"
+  },
+  {
+    "revision": "fea9c8b34d6e3b807e18",
+    "url": "/static/js/1.fea9c8b3.chunk.js"
+  },
+  {
+    "revision": "09ca05edf55c87b5d6d4",
+    "url": "/static/css/main.e5251117.chunk.css"
+  },
+  {
+    "revision": "41898fd55d906246369f2c0ca7a5e1aa",
+    "url": "/index.html"
+  }
+];
